b8fd7c52dd45f7f2b17fdbf2bb304f169011ac4d
Aditya062003 <adityavinay@iitbhilai.ac.in>
2023-07-16 22:09:03 +0530

    Tried Added map preview

--- lib/widgets/input_location.dart
+++ lib/widgets/input_location.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as loc;
+import 'package:geocoding/geocoding.dart';
+import 'package:places/models/place.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlng/latlng.dart';
 
 class InputLocation extends StatefulWidget {
   const InputLocation({super.key});
@@ -11,15 +15,29 @@ class InputLocation extends StatefulWidget {
 }
 
 class _InputLocationState extends State<InputLocation> {
-  Location? pickedLoacation;
+  PlaceLocation? _pickedLocation;
   var _isLoading = false;
+  Future<String> getAddressFromLatLng(double latitude, double longitude) async {
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(latitude, longitude);
+      Placemark place = placemarks[0];
+
+      String address =
+          "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
+      print(address);
+      return address;
+    } catch (e) {
+      return "Error: ${e.toString()}";
+    }
+  }
 
   void _getCurrentLocation() async {
-    Location location = Location();
+    loc.Location location = loc.Location();
 
     bool serviceEnabled;
-    PermissionStatus permissionGranted;
-    LocationData locationData;
+    loc.PermissionStatus permissionGranted;
+    loc.LocationData locationData;
 
     serviceEnabled = await location.serviceEnabled();
     if (!serviceEnabled) {
@@ -30,9 +48,9 @@ class _InputLocationState extends State<InputLocation> {
     }
 
     permissionGranted = await location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
+    if (permissionGranted == loc.PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
+      if (permissionGranted != loc.PermissionStatus.granted) {
         return;
       }
     }
@@ -40,7 +58,20 @@ class _InputLocationState extends State<InputLocation> {
       _isLoading = true;
     });
     locationData = await location.getLocation();
+    final double lat = locationData.latitude!;
+    final double lng = locationData.longitude!;
+    if(lat==null || lng == null){
+      return;
+    }
+    final address = await getAddressFromLatLng(lat, lng);
+    print(address);
+
     setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lng,
+        address: address,
+      );
       _isLoading = false;
     });
   }
@@ -48,14 +79,44 @@ class _InputLocationState extends State<InputLocation> {
   @override
   Widget build(BuildContext context) {
     Widget previewContent = Text(
-            'No Location chosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context)
-                .textTheme
-                .titleMedium!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),
-          );
-    if(_isLoading){
+      'No Location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context)
+          .textTheme
+          .titleMedium!
+          .copyWith(color: Theme.of(context).colorScheme.onBackground),
+    );
+    if (_pickedLocation != null && _pickedLocation!.latitude != null && _pickedLocation!.longitude != null) {
+      previewContent = FlutterMap(
+        options: MapOptions(
+          center: LatLng(_pickedLocation!.latitude!, _pickedLocation!.longitude!),
+          zoom: 13.0,
+        ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c'],
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: LatLng(_pickedLocation!.latitude!, _pickedLocation!.longitude!),
+                builder: (ctx) => Container(
+                  child: Icon(
+                    Icons.location_on,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 45,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
+    }
+    if (_isLoading) {
       previewContent = const CircularProgressIndicator();
     }
     return Column(
